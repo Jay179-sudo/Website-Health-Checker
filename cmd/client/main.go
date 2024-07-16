017f60443c00391f769c7e68436b17f106b3dc6e
Jay179-sudo <jayprasad2002@gmail.com>
2024-07-16 23:29:11 +0530

    feat: added mTLS for server and client
    Signed-off-by: Jay179-sudo <jayprasad2002@gmail.com>

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
 	"jaypd/healthcheck/rpc"
+	"log"
 	"log/slog"
 	"os"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
@@ -16,12 +20,44 @@ var (
 	logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 )
 
+func loadTLSCredentials() (credentials.TransportCredentials, error) {
+	// load the certificate of the CA who signed the server's certificate
+	pemServerCA, err := os.ReadFile("cert/ca-cert.pem")
+	if err != nil {
+		return nil, err
+	}
+	certPool := x509.NewCertPool()
+	if !certPool.AppendCertsFromPEM(pemServerCA) {
+		return nil, fmt.Errorf("failed to add server CA's certificate")
+	}
+	clientCert, err := tls.LoadX509KeyPair("cert/client-cert.pem", "cert/client-key.pem")
+	if err != nil {
+		return nil, err
+	}
+	// create creds and return
+	config := &tls.Config{
+		Certificates: []tls.Certificate{clientCert},
+		RootCAs:      certPool,
+	}
+
+	return credentials.NewTLS(config), nil
+
+}
+
 func main() {
 	address := flag.String("address", "0.0.0.0:8080", "default port where the client reaches out to the server")
+	enableTLS := flag.Bool("tls", true, "enable SSL/TLS")
 	url := flag.String("url", "https://google.com", "The URL which the gRPC client sends the request to")
 	flag.Parse()
-
-	conn, err := grpc.NewClient(*address, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	transportOption := grpc.WithTransportCredentials(insecure.NewCredentials())
+	if *enableTLS {
+		tlsCreds, err := loadTLSCredentials()
+		if err != nil {
+			log.Fatal("cannot load TLS credentials ", err)
+		}
+		transportOption = grpc.WithTransportCredentials(tlsCreds)
+	}
+	conn, err := grpc.NewClient(*address, transportOption)
 	if err != nil {
 		logger.Error("Client", "Error", err.Error())
 		return
